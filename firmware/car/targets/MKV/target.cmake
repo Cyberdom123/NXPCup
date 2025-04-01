321c2df6b9a1f22fda8d12ec7c322ba48bc626f3
Dominik Michalczyk <dmichalczyk73@gmail.com>
2025-04-01 19:54:37 +0200

    fixed distance mesurament

--- firmware/car/targets/MKV/target.cmake
+++ firmware/car/targets/MKV/target.cmake
@@ -46,3 +46,4 @@ add_custom_command(TARGET ${NAME}
         COMMAND ${CMAKE_OBJCOPY} "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${NAME}" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${NAME}.elf")
 
 target_stlink_flash(${NAME})
+target_jlink_flash(${NAME})
